43761e9f0f91321655a364a5056c8f0b587d784f
vincent <vincent87720@yahoo.com.tw>
2021-03-10 02:04:20 +0800

    Add: BatchGotAdmin function;

--- cmd/DepCmdGenerator/main.go
+++ cmd/DepCmdGenerator/main.go
@@ -37,6 +37,8 @@ func main() {
 
 		w := bufio.NewWriter(f)
 
+		batchGotAdminCmd(w)
+
 		file.WriteCmd(w, "netsh interface ip set address name=\""+settings.Interface+"\" source=static address="+ipFront+strconv.Itoa(ipCounter)+" mask="+settings.Mask+" gateway="+settings.Gateway+" gwmetric=1\r\n")
 		for idx, val := range settings.DNSServer {
 			if idx == 0 {
@@ -79,4 +81,34 @@ func parse(s string) (front string, startID int, err error) {
 	startID = int(int64id)
 
 	return front, startID, nil
+}
+
+func batchGotAdminCmd(w *bufio.Writer) {
+	file.WriteCmd(w, "@echo off\r\n")
+	file.WriteCmd(w, "\r\n")
+	file.WriteCmd(w, ":: BatchGotAdmin\r\n")
+	file.WriteCmd(w, ":-------------------------------------\r\n")
+	file.WriteCmd(w, "REM  --> Check for permissions\r\n")
+	file.WriteCmd(w, ">nul 2>&1 \"%SYSTEMROOT%\\system32\\cacls.exe\" \"%SYSTEMROOT%\\system32\\config\\system\"\r\n")
+	file.WriteCmd(w, "\r\n")
+	file.WriteCmd(w, "REM --> If error flag set, we do not have admin.\r\n")
+	file.WriteCmd(w, "if '%errorlevel%' NEQ '0' (\r\n")
+	file.WriteCmd(w, "    echo Requesting administrative privileges...\r\n")
+	file.WriteCmd(w, "    goto UACPrompt\r\n")
+	file.WriteCmd(w, ") else ( goto gotAdmin )\r\n")
+	file.WriteCmd(w, "\r\n")
+	file.WriteCmd(w, ":UACPrompt\r\n")
+	file.WriteCmd(w, "    echo Set UAC = CreateObject^(\"Shell.Application\"^) > \"%temp%\\getadmin.vbs\"\r\n")
+	file.WriteCmd(w, "    set params = %*:\"=\"\"\r\n")
+	file.WriteCmd(w, "    echo UAC.ShellExecute \"cmd.exe\", \"/c %~s0 %params%\", \"\", \"runas\", 1 >> \"%temp%\\getadmin.vbs\"\r\n")
+	file.WriteCmd(w, "\r\n")
+	file.WriteCmd(w, "    \"%temp%\\getadmin.vbs\"\r\n")
+	file.WriteCmd(w, "    del \"%temp%\\getadmin.vbs\"\r\n")
+	file.WriteCmd(w, "    exit /B\r\n")
+	file.WriteCmd(w, "\r\n")
+	file.WriteCmd(w, ":gotAdmin\r\n")
+	file.WriteCmd(w, "    pushd \"%CD%\"\r\n")
+	file.WriteCmd(w, "    CD /D \"%~dp0\"\r\n")
+	file.WriteCmd(w, ":--------------------------------------\r\n")
+	file.WriteCmd(w, "@echo on\r\n")
 }
